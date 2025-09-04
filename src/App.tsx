dd1331bea785c86432f3a4d35f35b89293b89a08
Ashley-Blu <matsekolengashley28@hmail.com>
2025-09-04 21:31:17 +0200

    Added the footer, and heading and repo picture for my Readme file

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Footer from './Components/Challenge';
+import styles from './Components/Challenge.module.css';
+
+function App(){
+    return <div> <Footer /> </div>
+}
+
+export default App;
---
+++ src/Components/Challenge.tsx
@@ -0,0 +1,8 @@
+
+function Footer(){
+  return <footer>
+    Bootcamp 2023 by <a href="https://mlab.co.za/">Codetribe</a>. Developed by <a href="#">Matsekoleng Ashley</a>.
+  </footer>
+}
+
+export default Footer
